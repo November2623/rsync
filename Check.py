93c777497a8a4793c40be2d7469bb427f7892d4d
Duy Tran <tranngocduytxqt98@gmail.com>
2018-11-03 14:46:19 +0700

    Hello

--- Check.py
+++
@@ -1,9 +0,0 @@
-import difflib
-
-file1 = 'Dest.txt'
-file2 = 'Note.txt'
-
-diff = difflib.ndiff(open(file1).readlines(),open(file2).readlines())
-re = ''.join(diff)
-print(re)
-print(type(re))
--- islink.py
+++
@@ -1,6 +0,0 @@
-import os.path
-def islink():
-    if os.path.islink('File2'):
-        return True
-    return False
-print(islink())
